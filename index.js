96ac65a7eb5b62e2c25ce214e48466a976b5ed9f
Kaustubh Alandkar <kaustubhalandkar@gmail.com>
2024-04-10 17:33:10 +0530

    update: challenge completed

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var multer = require('multer');
+require('dotenv').config();
+
+// Multer storage configuration
+const storageConf = multer.memoryStorage();
+
+// Initialize multer middleware with configured storage
+const upload = multer({ storage: storageConf });
 
 var app = express();
 
@@ -11,10 +18,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// Endpoint to handle file upload
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const fileInfo = {
+    name: req.file.originalname, // File name
+    type: req.file.mimetype, // File MIME type
+    size: req.file.size, // File size in bytes
+  };
+  res.json(fileInfo);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
